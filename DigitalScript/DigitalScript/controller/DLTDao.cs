5bab173bfa551ae758f8308dd8515eb247afc64c
Bob Lin <azrael830102@gmail.com>
2021-02-04 19:11:18 +0800

    Adjust db connection implementation way

--- DigitalScript/DigitalScript/controller/DLTDao.cs
+++ DigitalScript/DigitalScript/controller/DLTDao.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 
 namespace DigitalScript
@@ -8,14 +9,40 @@ namespace DigitalScript
     /// </summary>
     class DLTDao
     {
+        MySqlConnection conn;
+
+        /// <summary>
+        /// After done with DB access remember to close the connection
+        /// </summary>
+        DLTDao()
+        {
+            conn = DBTools.GetConnection();
+            try
+            {
+                conn.Open();
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Close the connecetion
+        /// </summary>
+        public void CloseConnection()
+        {
+            conn.Close();
+        }
+
         /// <summary>
         /// Get films
         /// </summary>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetFilms()
+        public List<Dictionary<string, object>> GetFilms()
         {
             string sql = "SELECT * FROM films";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -23,10 +50,10 @@ namespace DigitalScript
         /// </summary>
         /// <param name="group_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetOwnFilms(string group_Id)
+        public List<Dictionary<string, object>> GetOwnFilms(string group_Id)
         {
             string sql = "SELECT * FROM films WHERE group_id = '" + group_Id + "'";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -34,20 +61,20 @@ namespace DigitalScript
         /// </summary>
         /// <param name="subject_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetSubjectFilms(string subject_Id)
+        public List<Dictionary<string, object>> GetSubjectFilms(string subject_Id)
         {
             string sql = "SELECT * FROM films WHERE subjects_id = " + subject_Id;
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
         /// Get subject
         /// </summary>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetSubject()
+        public List<Dictionary<string, object>> GetSubject()
         {
             string sql = "SELECT * FROM subjects";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -55,7 +82,7 @@ namespace DigitalScript
         /// </summary>
         /// <param name="film_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetScenes(string film_Id)
+        public List<Dictionary<string, object>> GetScenes(string film_Id)
         {
             string sql = "SELECT scenes.*, backgrounds.name as BgImg, foregrounds.name as FgImg,props.name as PropImg,masks.name as MaskImg ,clothes.name as ClothImg,puppets.name as PuppetImg " +
                               "FROM(select * from `film_scenesorder`where `film_id`= '" + film_Id + "') temp " +
@@ -67,7 +94,7 @@ namespace DigitalScript
                               "LEFT JOIN masks ON scenes.`mask_id` = masks.id " +
                               "LEFT JOIN puppets ON scenes.`puppet_id` = puppets.id " +
                                 "order by `scenesorder`";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -75,14 +102,14 @@ namespace DigitalScript
         /// </summary>
         /// <param name="scene_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetScenesSounds(string scene_Id)
+        public List<Dictionary<string, object>> GetScenesSounds(string scene_Id)
         {
             string sql = "SELECT sounds.* " +
                             "FROM scene_soundsorder " +
                             "JOIN sounds ON scene_soundsorder.sound_id = sounds.id " +
                                 "WHERE scene_id = '" + scene_Id + "' " +
                                 "ORDER BY scene_soundsorder.soundsorder ASC";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -90,10 +117,10 @@ namespace DigitalScript
         /// </summary>
         /// <param name="material_type"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetMaterials(MaterialType material_type)
+        public List<Dictionary<string, object>> GetMaterials(MaterialType material_type)
         {
             string sql = "SELECT * FROM " + CustomType.ToString(material_type);
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -102,10 +129,10 @@ namespace DigitalScript
         /// <param name="material_type"></param>
         /// <param name="group_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetOwnMaterials(MaterialType material_type, string group_Id)
+        public List<Dictionary<string, object>> GetOwnMaterials(MaterialType material_type, string group_Id)
         {
             string sql = "SELECT * FROM " + CustomType.ToString(material_type) + " WHERE group_id =" + group_Id;
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
 
@@ -114,24 +141,24 @@ namespace DigitalScript
         /// </summary>
         /// <param name="scene_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetDialogues(string scene_Id)
+        public List<Dictionary<string, object>> GetDialogues(string scene_Id)
         {
             string sql = "SELECT dialogues.* " +
                             "FROM dialogues " +
                             "JOIN characters_dialogues ON dialogues.id = characters_dialogues.dialogues_id " +
                             "JOIN characters ON characters.id = characters_dialogues.character_id " +
                                 "WHERE characters.scene_id = '" + scene_Id + "'";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
         /// Get all sounds
         /// </summary>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetSounds()
+        public List<Dictionary<string, object>> GetSounds()
         {
             string sql = "SELECT * FROM sounds ORDER BY id DESC";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
         /// <summary>
@@ -139,40 +166,40 @@ namespace DigitalScript
         /// </summary>
         /// <param name="group_Id"></param>
         /// <returns></returns>
-        public static List<Dictionary<string, object>> GetOwnSounds(string group_Id)
+        public List<Dictionary<string, object>> GetOwnSounds(string group_Id)
         {
             string sql = "SELECT * FROM sounds WHERE group_id = " + group_Id + " ORDER BY id DESC";
-            return DBTools.Query(sql);
+            return DBTools.Query(sql, conn);
         }
 
 
 
-        public static Script GetScript(string id)
+        public Script GetScript(string id)
         {
             Script result = new Script();
             //...
             return result;
         }
 
-        public static Scene GetScene(string id)
+        public Scene GetScene(string id)
         {
             Scene result = new Scene();
             //...
             return result;
         }
 
-        public static Actor GetActor(Guid id) 
+        public Actor GetActor(Guid id)
         {
             Actor result = new Actor();
             //...
-            return result; 
+            return result;
         }
 
-        static void Main(string[] args)
+        /*static void Main(string[] args)
         {
             List<Dictionary<string, object>> lst = GetFilms();
 
             Console.WriteLine();
-        }
+        }*/
     }
 }
